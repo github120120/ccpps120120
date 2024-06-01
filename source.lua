6604ebe6c914e36aabc7b845c1a975594723de4c
github120120 <145911117+github120120@users.noreply.github.com>
2024-06-01 08:48:47 -0400

    Update source.lua

--- source.lua
+++ source.lua
@@ -3,12 +3,13 @@ local customerScreen = script.Parent.CustomerMonitor.Screen.SurfaceGui.POSUI
 local transaction = false
 local open = false
 local scan = script.Parent.StaffMonitor.Scan.TouchPart
+print("running")
 
 --Log In script
 scan.Touched:Connect(function(tool)
 
 	local staffID = tool["POINTPLUS_STAFF"]
-
+	print("touched")
 	if open == false and staffID.Value == true then
 		open = true
 		staffScreen.Visible = true
