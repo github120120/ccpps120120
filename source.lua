54e66e2354a1ebaa267d506b15392af7e7216799
github120120 <145911117+github120120@users.noreply.github.com>
2024-06-02 15:22:37 -0400

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,26 +1,25 @@
 local MyModule = {}
-	local staffScreen = script.Parent.StaffMonitor.Screen.SurfaceGui.POSUI.Main
-	local customerScreen = script.Parent.CustomerMonitor.Screen.SurfaceGui.POSUI
-	local transaction = false
-	local open = false
-	local scan = script.Parent.StaffMonitor.Scan.TouchPart
-	print("running")
-	
-	--Log In script
-	scan.Touched:Connect(function(tool)
-	
-		local staffID = tool["POINTPLUS_STAFF"]
-		print("touched")
-		if open == false and staffID.Value == true then
-			open = true
-			staffScreen.Visible = true
-			script.Parent.Beep:Play()
-	
-		end
-	
-	end)
+
+-- Define your variables and functions here
+MyModule.staffScreen = script.Parent.StaffMonitor.Screen.SurfaceGui.POSUI.Main
+MyModule.customerScreen = script.Parent.CustomerMonitor.Screen.SurfaceGui.POSUI
+MyModule.transaction = false
+MyModule.open = false
+MyModule.scan = script.Parent.StaffMonitor.Scan.TouchPart
+
+-- Log In script
+MyModule.scan.Touched:Connect(function(tool)
+    local staffID = tool["POINTPLUS_STAFF"]
+    print("touched")
+    if MyModule.open == false and staffID.Value == true then
+        MyModule.open = true
+        MyModule.staffScreen.Visible = true
+        script.Parent.Beep:Play()
+    end
+end)
 
 return MyModule
+
 
 
 
