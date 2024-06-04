b5fb8656b0fffe627f9cc9a2d388c968048bbb01
github120120 <145911117+github120120@users.noreply.github.com>
2024-06-04 15:47:06 -0400

    Update POSScript.lua

--- POSScript.lua
+++ POSScript.lua
@@ -1,5 +1,36 @@
 local POSModule = {}
 
+-- Ensures initial state is set up correctly
+function POSModule.init(state)
+    state.open = false
+    state.onLogIn = false
+    state.transaction = false
+    state.totalPrice = 0
+    state.typingPin = false
+    state.contactless = false
+    state.canInsert = false
+    state.isInsert = false
+    state.isContactless = false
+    state.customerUIOpen = false
+
+    -- Connect events
+    state.scan.Touched:Connect(function(tool)
+        if tool:FindFirstChild("POINTPLUS_STAFF") then
+            POSModule.logIn(tool, state)
+        elseif tool:FindFirstChild("POINTPLUS_ITEM") then
+            POSModule.scanItem(tool, state)
+        end
+    end)
+    state.staffScreen.Locked.LogInButton.MouseButton1Click:Connect(function() POSModule.logInScreen(state) end)
+    state.staffScreen.LogIn.BackButton.MouseButton1Click:Connect(function() POSModule.goBack(state) end)
+    state.staffScreen.Main.LogOut.MouseButton1Click:Connect(function() POSModule.logOut(state) end)
+    state.main.Buttons.Void.MouseButton1Click:Connect(function() POSModule.voidTransaction(state) end)
+    state.main.Buttons.Card.MouseButton1Click:Connect(function() POSModule.cardTransaction(state) end)
+    state.cardReader.InsertCard.ClickDetector.MouseClick:Connect(function() POSModule.cardInsert(state) end)
+    state.cardReader.Tap.Touched:Connect(function(card) POSModule.cardTap(card, state) end)
+    state.cardReader.Touch.ClickDetector.MouseClick:Connect(function() POSModule.typePin(state) end)
+end
+
 function POSModule.logIn(tool, state)
     local staffID = tool["POINTPLUS_STAFF"]
     if staffID and state.onLogIn == true then
@@ -10,30 +41,9 @@ function POSModule.logIn(tool, state)
         state.customerScreen.Main.ItemList.Template.Visible = false
         state.customerScreen.Main.ItemScanned.ItemScannedName.Text = "---"
         state.customerScreen.Main.ItemScanned.ItemScannedPrice.Text = "---"
-
-        state.customerScreen.Closed.Visible = false
-        state.customerScreen.Main.Visible = true
-    end
-end
-
---[[
-function POSModule.logIn(tool, state)
-    local staffID = tool["POINTPLUS_STAFF"]
-    if staffID and state.open == false and state.onLogIn == true then
-        state.open = true
-        state.staffScreen.Main.Visible = true
-        state.POS.Beep:Play()
-        state.main.Items.ItemList.Template.Visible = false
-        state.customerScreen.Main.ItemList.Template.Visible = false
-        state.customerScreen.Main.ItemScanned.ItemScannedName.Text = "---"
-        state.customerScreen.Main.ItemScanned.ItemScannedPrice.Text = "---"
-        state.customerScreen.Closed.Visible = false
-        state.customerScreen.Main.Visible = true
         POSModule.showCustomerUI(state)
     end
 end
---]]
-
 
 function POSModule.showCustomerUI(state)
     state.customerScreen.Closed.Visible = false
@@ -78,6 +88,8 @@ function POSModule.logOut(state)
     -- Save the state of the customer UI
     state.customerUIOpen = false
 end
+
+-- Other functions remain the same...
 
 function POSModule.scanItem(item, state)
     if state.open == true and state.transaction == false then
@@ -240,27 +252,6 @@ function POSModule.cardTap(card, state)
         state.transaction = false
         POSModule.voidTransaction(state)
     end
-end
-
-function POSModule.init(state)
-    -- Connect events
-    state.scan.Touched:Connect(function(tool)
-        if tool:FindFirstChild("POINTPLUS_STAFF") then
-            POSModule.logIn(tool, state)
-            state.customerScreen.Closed.Visible = false
-            state.customerScreen.Main.Visible = true
-        elseif tool:FindFirstChild("POINTPLUS_ITEM") then
-            POSModule.scanItem(tool, state)
-        end
-    end)
-    state.staffScreen.Locked.LogInButton.MouseButton1Click:Connect(function() POSModule.logInScreen(state) end)
-    state.staffScreen.LogIn.BackButton.MouseButton1Click:Connect(function() POSModule.goBack(state) end)
-    state.staffScreen.Main.LogOut.MouseButton1Click:Connect(function() POSModule.logOut(state) end)
-    state.main.Buttons.Void.MouseButton1Click:Connect(function() POSModule.voidTransaction(state) end)
-    state.main.Buttons.Card.MouseButton1Click:Connect(function() POSModule.cardTransaction(state) end)
-    state.cardReader.InsertCard.ClickDetector.MouseClick:Connect(function() POSModule.cardInsert(state) end)
-    state.cardReader.Tap.Touched:Connect(function(card) POSModule.cardTap(card, state) end)
-    state.cardReader.Touch.ClickDetector.MouseClick:Connect(function() POSModule.typePin(state) end)
 end
 
 return POSModule
